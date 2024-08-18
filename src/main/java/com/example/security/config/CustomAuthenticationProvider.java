1bba9d10cdb672e454d00329b28c056c9d0fa9e1
박성현 <qkr970504@gmail.com>
2024-08-18 22:34:11 +0900

    AuthenticationProvider 구현
    - 입력받은 사용자 정보 조회
    - 인증 성공 시 토큰 생성
    - AuthenticationManage로 반환

---
+++ src/main/java/com/example/security/config/CustomAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package com.example.security.config;
+
+import com.example.security.domain.member.entity.MemberDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Log4j2
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        final UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        // AuthenticaionFilter에서 생성된 토큰으로부터 아이디와 비밀번호를 조회함
+        final String memberId = token.getName();
+        final String pwd = (String) token.getCredentials();
+
+        // UserDetailsService를 통해 DB에서 아이디로 사용자 조회
+        final MemberDetails memberDetails = (MemberDetails) userDetailsService.loadUserByUsername(memberId);
+        if (!passwordEncoder.matches(pwd, memberDetails.getPassword())) {
+            throw new BadCredentialsException(memberDetails.getUsername() + "Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(memberDetails, pwd, memberDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
